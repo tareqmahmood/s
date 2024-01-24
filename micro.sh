58a79427ac2289afc4f71e7943f3cbc636d235df
Md. Tareq Mahmood <tareq.py@gmail.com>
2024-01-24 15:48:39 -0600

    micro editor redirect

---
+++ micro.sh
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<!-- This is hacky :p -->
+<meta charset="utf-8">
+<title>Redirecting to https://getmic.ro/</title>
+<meta http-equiv="refresh" content="0; URL=https://getmic.ro/">
+<link rel="canonical" href="https://getmic.ro/">
