27a44084dd68040a923b0cb7f46867cb7010c8a8
Md. Tareq Mahmood <tareq.py@gmail.com>
2024-01-24 18:27:15 -0600

    micro installation

--- micro.sh
+++ micro.sh
@@ -1,6 +1,7 @@
-<!DOCTYPE html>
-<!-- This is hacky :p -->
-<meta charset="utf-8">
-<title>Redirecting to https://getmic.ro/</title>
-<meta http-equiv="refresh" content="0; URL=https://getmic.ro/">
-<link rel="canonical" href="https://getmic.ro/">
+sudo apt update
+sudo apt install -y wget
+mkdir -p ~/.micro
+wget https://getmic.ro/ -O install.sh
+bash install.sh
+rm install.sh
+sudo mv ./micro /usr/bin/micro
