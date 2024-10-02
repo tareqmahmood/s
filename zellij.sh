d680e3c47b63dd0c22664d1a7c02a91b69537395
Md. Tareq Mahmood <tareq.py@gmail.com>
2024-10-01 23:12:02 -0500

    zellij update

--- zellij.sh
+++ zellij.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo apt install -y wget
 mkdir -p ~/.zellij
-wget https://github.com/zellij-org/zellij/releases/download/v0.39.2/zellij-x86_64-unknown-linux-musl.tar.gz -O ~/.zellij/zellij.tar.gz
+wget https://github.com/zellij-org/zellij/releases/download/v0.40.1/zellij-x86_64-unknown-linux-musl.tar.gz -O ~/.zellij/zellij.tar.gz
 tar -xvf ~/.zellij/zellij.tar.gz -C ~/.zellij
 chmod u+x ~/.zellij/zellij
 sudo cp ~/.zellij/zellij /usr/local/bin/
