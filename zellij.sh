75e6dfe0b0228f5698ccc7bde9a76f71b351db25
Md. Tareq Mahmood <tareq.py@gmail.com>
2025-03-19 17:06:29 -0500

    zellij downgraded due to flicker issue

--- zellij.sh
+++ zellij.sh
@@ -1,14 +1,8 @@
 sudo apt update
 sudo apt install -y wget
 mkdir -p ~/.zellij
-wget https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz -O ~/.zellij/zellij.tar.gz
+wget https://github.com/zellij-org/zellij/releases/download/v0.41.2/zellij-x86_64-unknown-linux-musl.tar.gz -O ~/.zellij/zellij.tar.gz
 tar -xvf ~/.zellij/zellij.tar.gz -C ~/.zellij
 chmod u+x ~/.zellij/zellij
 sudo cp ~/.zellij/zellij /usr/local/bin/
 rm -r ~/.zellij/
-
-echo "Adding alias"
-echo "Sync tab: zsync"
-echo "alias zsync='zellij action toggle-active-sync-tab'" >> ~/.bashrc
-echo "source ~/.bashrc to take effect"
-echo ''
